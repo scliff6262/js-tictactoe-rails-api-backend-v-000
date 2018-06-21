e567ff1c88b5759c74b0e529d1f8f4f17821c2cb
Sean Clifford <sclifford6262@gmail.com>
2018-06-21 18:25:49 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,11 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  def create
 
+  end
+
+  private
+
+  def game_params
+    params.require(:product).permit(:state)
+  end
 end
