adc5526be542639640056258fbddaa39e3b98524
Sean Clifford <sclifford6262@gmail.com>
2018-06-21 18:40:14 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,6 @@ class GamesController < ApplicationController
   #before_action :set_game
 
   def create
-    binding.pry
     @game = Game.create(state: game_params)
     render json: @game if @game.save
   end
